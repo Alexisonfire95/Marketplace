c6d0c1732c1f5ea960531cc9a29cd39120b38208
Alexisonfire95 <vini.marmone@gmail.com>
2025-12-17 15:24:08 +0200

    fix(db): activate MikroORM module and configure async connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,20 +1,20 @@
-import { Module } from "@nestjs/common"
-import { ConfigModule, ConfigService } from "@nestjs/config"
-import { MikroOrmModule } from "@mikro-orm/nestjs"
-import { PostgreSqlDriver } from "@mikro-orm/postgresql"
-import { config } from "./config/config"
-import { getMikroOrmConfig } from "./config/database.config"
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { MikroOrmModule } from "@mikro-orm/nestjs";
+import { PostgreSqlDriver } from "@mikro-orm/postgresql";
+import { config } from "./config/config";
+import { getMikroOrmConfig } from "./config/database.config";
 
 @Module({
   imports: [
-    ConfigModule.forRoot(config)
-    //     MikroOrmModule.forRootAsync({
-    //       imports: [ConfigModule],
-    //       useFactory: getMikroOrmConfig,
-    //       driver: PostgreSqlDriver,
-    //       inject: [ConfigService]
-    //     })
+    ConfigModule.forRoot(config),
+    MikroOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      useFactory: getMikroOrmConfig,
+      driver: PostgreSqlDriver,
+      inject: [ConfigService],
+    }),
   ],
-  exports: [ConfigModule]
+  exports: [ConfigModule],
 })
 export class AppModule {}
